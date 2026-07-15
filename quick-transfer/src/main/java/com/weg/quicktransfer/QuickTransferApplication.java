583704ac4def1d9eb3c82b5b100560cc5f79380b
EDUARDO TAMBORELLI FERREIRA LINO <eduardo_lino@estudante.sesisenai.org.br>
2026-07-15 09:31:20 -0300

    feat: Creating the back-end and folder structure.

---
+++ quick-transfer/src/main/java/com/weg/quicktransfer/QuickTransferApplication.java
@@ -0,0 +1,13 @@
+package com.weg.quicktransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickTransferApplication.class, args);
+	}
+
+}
