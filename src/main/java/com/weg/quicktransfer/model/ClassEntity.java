c65449d7b6a75a72cb28487c7984c23c0a1936ae
BrunoAthanazio <athanazioferreirabruno@gmail.com>
2026-07-16 12:33:46 -0300

    feat: create ClassEntity model

--- src/main/java/com/weg/quicktransfer/model/ClassEntity.java
+++ src/main/java/com/weg/quicktransfer/model/ClassEntity.java
@@ -1,4 +1,43 @@
 package com.weg.quicktransfer.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "class_entity")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class ClassEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @Column(name = "finish_date", nullable = false)
+    private LocalDate finishDate;
+
+    @OneToMany(mappedBy = "classEntity", cascade = CascadeType.ALL)
+    List<Student> students;
+
+    @Column(nullable = false)
+    private String acronym;
+
+    public ClassEntity(Course course, LocalDate finishDate, List<Student> students, String acronym) {
+        this.course = course;
+        this.finishDate = finishDate;
+        this.students = students;
+        this.acronym = acronym;
+    }
 }
