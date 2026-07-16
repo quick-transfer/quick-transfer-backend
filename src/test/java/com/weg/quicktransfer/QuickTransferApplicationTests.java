7ff89cc25b3dbaf784ef546a78129f32b8deea97
Kilop206 <guilhermextra118@gmail.com>
2026-07-16 08:38:02 -0300

    chore: configure application.properties and update .gitignore to support .env

--- src/test/java/com/weg/quicktransfer/QuickTransferApplicationTests.java
+++ src/test/java/com/weg/quicktransfer/QuickTransferApplicationTests.java
@@ -2,8 +2,10 @@ package com.weg.quicktransfer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class QuickTransferApplicationTests {
 
 	@Test
