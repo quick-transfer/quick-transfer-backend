b22c27e11031973d71ad0b6d263952bc79f0b191
Kilop206 <guilhermextra118@gmail.com>
2026-07-17 09:10:30 -0300

    feat: create userPrincipal

--- src/main/java/com/weg/quicktransfer/security/UserPrincipal.java
+++ src/main/java/com/weg/quicktransfer/security/UserPrincipal.java
@@ -1,4 +1,59 @@
 package com.weg.quicktransfer.security;
 
-public class UserPrincipal {
+import com.weg.quicktransfer.model.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+public class UserPrincipal implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().toString()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserName();
+    }
+
+    public String getName() {
+        return user.getName();
+    }
+
+    public Long getId() {
+        return user.getId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
