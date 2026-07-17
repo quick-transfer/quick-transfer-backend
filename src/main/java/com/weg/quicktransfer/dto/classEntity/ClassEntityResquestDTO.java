ed44793556a1d1e89371e43e7f2cd699dc2cb1c7
BrunoAthanazio <athanazioferreirabruno@gmail.com>
2026-07-17 10:16:10 -0300

    feat: dto validation on ClassEntityResquestDTO

--- src/main/java/com/weg/quicktransfer/dto/classEntity/ClassEntityResquestDTO.java
+++ src/main/java/com/weg/quicktransfer/dto/classEntity/ClassEntityResquestDTO.java
@@ -1,10 +1,20 @@
 package com.weg.quicktransfer.dto.classEntity;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 import java.time.LocalDate;
 
 public record ClassEntityResquestDTO(
-    Long courseId,
-    LocalDate finishDate,
-    String acronym
+
+        @NotBlank(message = "Course id must not be empty")
+        Long courseId,
+
+        @NotBlank(message = "finish date must not be empty")
+        LocalDate finishDate,
+
+        @NotBlank(message = "Class acronym must not be empty")
+        @Pattern(regexp = "^[A-Z0-9-]+$")
+        String acronym
 ) {
 }
