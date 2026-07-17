89cd903863a39124f011ff1380845090b903c93b
BrunoAthanazio <athanazioferreirabruno@gmail.com>
2026-07-17 08:32:53 -0300

    feat: create admin model

---
+++ src/main/java/com/weg/quicktransfer/model/Admin.java
@@ -0,0 +1,21 @@
+package com.weg.quicktransfer.model;
+
+import com.weg.quicktransfer.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "admins")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Admin extends User{
+
+    public Admin(String name, String userName, String email, String password, Role role) {
+        super(name, userName, email, password, role);
+    }
+}
