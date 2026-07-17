23ca3bc50b8a45eebd6878f0035cf66ca77ff1b0
BrunoAthanazio <athanazioferreirabruno@gmail.com>
2026-07-17 12:42:35 -0300

    fix: added @SuperBuilder annotation to Admin model

--- src/main/java/com/weg/quicktransfer/model/Admin.java
+++ src/main/java/com/weg/quicktransfer/model/Admin.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Table(name = "admins")
@@ -13,6 +14,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@SuperBuilder
 public class Admin extends User{
 
     public Admin(String name, String userName, String email, String password) {
