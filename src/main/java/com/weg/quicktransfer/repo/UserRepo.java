68655dd50c8301235dbb4b9fa6b94895f164652f
Kilop206 <guilhermextra118@gmail.com>
2026-07-17 08:50:11 -0300

    feat: create CustomUserDetailsService

--- src/main/java/com/weg/quicktransfer/repo/UserRepo.java
+++ src/main/java/com/weg/quicktransfer/repo/UserRepo.java
@@ -1,4 +1,12 @@
 package com.weg.quicktransfer.repo;
 
-public class UserRepo {
+import com.weg.quicktransfer.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
