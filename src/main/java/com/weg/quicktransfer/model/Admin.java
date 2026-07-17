d9f0b3f31b386e3b928c6d3690a97b91a50f1a1a
Eduardo Tamborelli Ferreira Lino <eduardo_lino@estudante.sesisenai.org.br>
2026-07-17 11:23:59 -0300

    Remove unused import for AllArgsConstructor in Admin.java

--- src/main/java/com/weg/quicktransfer/model/Admin.java
+++ src/main/java/com/weg/quicktransfer/model/Admin.java
@@ -2,7 +2,6 @@ package com.weg.quicktransfer.model;
 
 import com.weg.quicktransfer.enums.Role;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
