40f479a540d497c59fe12a89c344ae7e94df5ef3
BrunoAthanazio <athanazioferreirabruno@gmail.com>
2026-07-20 10:28:32 -0300

    feat: added startDate to ClassEntity DTOs

--- src/main/java/com/weg/quicktransfer/dto/classEntity/ClassEntityResponseDTO.java
+++ src/main/java/com/weg/quicktransfer/dto/classEntity/ClassEntityResponseDTO.java
@@ -8,6 +8,7 @@ import java.util.List;
 public record ClassEntityResponseDTO(
         Long id,
         String courseName,
+        LocalDate startDate,
         LocalDate finishDate,
         String acronym,
         List<Student> students
