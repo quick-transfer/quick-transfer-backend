a793100f389bc6ce7ee4a9795a909ecbf548e80d
EDUARDO TAMBORELLI FERREIRA LINO <eduardo_lino@estudante.sesisenai.org.br>
2026-07-21 09:16:11 -0300

    fix: Create the new attribute for the student

--- src/main/java/com/weg/quicktransfer/dto/student/StudentResponseDTO.java
+++ src/main/java/com/weg/quicktransfer/dto/student/StudentResponseDTO.java
@@ -4,6 +4,7 @@ public record StudentResponseDTO(
     Long id,
     String name,
     String email,
+    Long age,
     Double averageGrade,
     String acronym,
     String course,
--- src/main/java/com/weg/quicktransfer/model/Student.java
+++ src/main/java/com/weg/quicktransfer/model/Student.java
@@ -24,6 +24,9 @@ public class Student{
     @Column(nullable = false)
     private String email;
 
+    @Column(nullable = false)
+    private Long age;
+
     @Column(name = "average_grade")
     private Double averageGrade;
 
@@ -41,10 +44,11 @@ public class Student{
     @OneToOne(mappedBy = "student")
     private Interview interview;
 
-    public Student(String name, String email, Double averageGrade, ClassEntity classEntity,
+    public Student(String name, String email, Long age, Double averageGrade, ClassEntity classEntity,
             StudentInterviewStatus status, Boolean hasSeenEmail, Interview interview) {
         this.name = name;
         this.email = email;
+        this.age = age;
         this.averageGrade = averageGrade;
         this.classEntity = classEntity;
         this.status = status;
