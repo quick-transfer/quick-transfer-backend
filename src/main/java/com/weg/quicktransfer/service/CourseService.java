76b4e579c1fac1f138a0b2886d0211951c08d66e
BrunoAthanazio <athanazioferreirabruno@gmail.com>
2026-07-21 12:46:56 -0300

    fix: fixed coordinator FindById

--- src/main/java/com/weg/quicktransfer/service/CourseService.java
+++ src/main/java/com/weg/quicktransfer/service/CourseService.java
@@ -3,10 +3,13 @@ package com.weg.quicktransfer.service;
 import com.weg.quicktransfer.dto.course.CourseRequestDTO;
 import com.weg.quicktransfer.dto.course.CourseResponseDTO;
 import com.weg.quicktransfer.exception.PlaceNotFoundException;
+import com.weg.quicktransfer.exception.UserNotFoundException;
+import com.weg.quicktransfer.mapper.CoordinatorMapper;
 import com.weg.quicktransfer.mapper.CourseMapper;
 import com.weg.quicktransfer.model.ClassEntity;
 import com.weg.quicktransfer.model.Coordinator;
 import com.weg.quicktransfer.model.Course;
+import com.weg.quicktransfer.repo.CoordinatorRepository;
 import com.weg.quicktransfer.repo.CourseRepository;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +24,7 @@ public class CourseService {
 
     private final CourseMapper courseMapper;
     private final CourseRepository courseRepository;
-    private final CoordinatorService coordinatorService;
+    private final CoordinatorRepository coordinatorRepository;
 
     //returns a new Course
     public CourseResponseDTO create(CourseRequestDTO courseRequestDTO){
@@ -30,9 +33,21 @@ public class CourseService {
             throw new CourseNotFoundException("Course already exists with this name");
         }
         //finds coordinator by id
-        Coordinator coordinator = coordinatorService.findById(courseRequestDTO.coordinatorId());
+        if(courseRequestDTO.coordinatorId() <= 0) {
+            throw new IllegalArgumentException("Id can not be less than 1");
+        }
+
+        Coordinator coordinator = coordinatorRepository.findById(courseRequestDTO.coordinatorId()).orElseThrow(() -> new UserNotFoundException(courseRequestDTO.coordinatorId()));
+        //initializes list
+        List<String> coursesName = new ArrayList<>();
+
+        //get courses names
+        for(Course course : coordinator.getCourses()){
+            coursesName.add(course.getName());
+        }
         //transform CourseRequestDTO to entity
         Course course = courseMapper.toEntity(courseRequestDTO, coordinator);
+
         //saves course
         courseRepository.save(course);
         List<String> classesAcronym = new ArrayList<>();
