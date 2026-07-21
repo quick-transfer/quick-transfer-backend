9888cea72c33c5c0aa9425aca308ac115714d048
BrunoAthanazio <athanazioferreirabruno@gmail.com>
2026-07-21 10:12:51 -0300

    feat: method findAll of CourseService

--- src/main/java/com/weg/quicktransfer/service/CourseService.java
+++ src/main/java/com/weg/quicktransfer/service/CourseService.java
@@ -4,6 +4,7 @@ import com.weg.quicktransfer.dto.course.CourseRequestDTO;
 import com.weg.quicktransfer.dto.course.CourseResponseDTO;
 import com.weg.quicktransfer.exception.PlaceNotFoundException;
 import com.weg.quicktransfer.mapper.CourseMapper;
+import com.weg.quicktransfer.model.ClassEntity;
 import com.weg.quicktransfer.model.Coordinator;
 import com.weg.quicktransfer.model.Course;
 import com.weg.quicktransfer.repo.CourseRepository;
@@ -33,5 +34,18 @@ public class CourseService {
         courseRepository.save(course);
         List<String> classesAcronym = new ArrayList<>();
         return courseMapper.toResponse(course, classesAcronym);
+    }
+
+    public List<CourseResponseDTO> findAll(){
+        List<Course> courses = courseRepository.findAll();
+        List<CourseResponseDTO> courseResponseDTOS = new ArrayList<>();
+        for(Course course : courses){
+            List<String> classesAcronym = new ArrayList<>();
+            for (ClassEntity classEntity : course.getClasses()){
+                classesAcronym.add(classEntity.getAcronym());
+            }
+            courseResponseDTOS.add(courseMapper.toResponse(course, classesAcronym));
+        }
+        return courseResponseDTOS;
     }
 }
