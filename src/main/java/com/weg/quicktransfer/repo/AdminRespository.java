2a01d15955a91245190eadbc3cb4c01c212b7cca
VINICIUS DERETTI <vinicius_deretti@estudante.sesisenai.org.br>
2026-07-21 07:44:11 -0300

    feat: @repository removed in Admin and Coordinator

--- src/main/java/com/weg/quicktransfer/repo/AdminRespository.java
+++ src/main/java/com/weg/quicktransfer/repo/AdminRespository.java
@@ -4,7 +4,6 @@ import com.weg.quicktransfer.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface AdminRespository extends JpaRepository<Admin, Long> {
     boolean existsByUsername(String username);
 }
--- src/main/java/com/weg/quicktransfer/repo/CoordinatorRepository.java
+++ src/main/java/com/weg/quicktransfer/repo/CoordinatorRepository.java
@@ -4,7 +4,6 @@ import com.weg.quicktransfer.model.Coordinator;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CoordinatorRepository extends JpaRepository<Coordinator, Long> {
     boolean existsByUsername(String Username);
 }
