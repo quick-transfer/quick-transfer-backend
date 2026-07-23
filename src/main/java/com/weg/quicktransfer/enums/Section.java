bad749d5df2a2e87c917fde5cae63edfca36b8b5
Eduardo Tamborelli Ferreira Lino <eduardo_lino@estudante.sesisenai.org.br>
2026-07-23 08:03:13 -0300

    Refactor Section enum by cleaning up comments
    Removed unnecessary comment and formatted the TI enum entry.

--- src/main/java/com/weg/quicktransfer/enums/Section.java
+++ src/main/java/com/weg/quicktransfer/enums/Section.java
@@ -1,7 +1,7 @@
 package com.weg.quicktransfer.enums;
-
+// Esse arquivo é para cada setor e não para área toda por favor realizar alteração
 public enum Section {
     BOBINAGEM,
     MANUTENCAO,
-    TI
+    TI 
 }
