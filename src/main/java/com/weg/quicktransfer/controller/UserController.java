101e8d184ae8a73077306ab1966f4dec7e083af0
Kilop206 <guilhermextra118@gmail.com>
2026-07-23 12:41:08 -0300

    feat: implement user controller and service

--- src/main/java/com/weg/quicktransfer/controller/UserController.java
+++ src/main/java/com/weg/quicktransfer/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.weg.quicktransfer.controller;
 
-import com.weg.quicktransfer.dto.admin.AdminRequestDTO;
-import com.weg.quicktransfer.dto.admin.AdminResponseDTO;
 import com.weg.quicktransfer.dto.user.UserResponseDTO;
 import com.weg.quicktransfer.dto.user.UserUpdateRequestDTO;
 import com.weg.quicktransfer.service.UserService;
@@ -19,37 +17,32 @@ public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/create/")
-    public ResponseEntity<UserResponseDTO> createCoordinator(@RequestBody AdminRequestDTO adminRequestDTO) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(adminRequestDTO));
-    }
-
     @GetMapping("/find/id/{id}")
-    public ResponseEntity<UserResponseDTO> findCoordinatorById(@PathVariable Long id) {
+    public ResponseEntity<UserResponseDTO> findUserById(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(userService.findById(id));
     }
 
     @GetMapping("/find/name/{name}")
-    public ResponseEntity<List<UserResponseDTO>> findCoordintatorByName(@PathVariable String name) {
+    public ResponseEntity<List<UserResponseDTO>> findUserByName(@PathVariable String name) {
         return ResponseEntity.status(HttpStatus.OK).body(userService.findByName(name));
     }
 
     @GetMapping("/find/all")
-    public ResponseEntity<List<UserResponseDTO>> findAllCoordinators() {
+    public ResponseEntity<List<UserResponseDTO>> findAllUsers() {
         return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<UserResponseDTO> updateCoordinator(
+    public ResponseEntity<UserResponseDTO> updateUser(
             @PathVariable Long id,
             @RequestBody UserUpdateRequestDTO userUpdateRequestDTO
             ) {
-        return ResponseEntity.status(HttpStatus.OK).body(userService.update(id, ));
+        return ResponseEntity.status(HttpStatus.OK).body(userService.update(id, userUpdateRequestDTO));
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Void> deleteCoordinator(@PathVariable Long id) {
-        userService.delete(id);
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteById(id);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
--- src/main/java/com/weg/quicktransfer/dto/user/UserUpdateRequestDTO.java
+++ src/main/java/com/weg/quicktransfer/dto/user/UserUpdateRequestDTO.java
@@ -9,12 +9,6 @@ import jakarta.validation.constraints.Size;
 public record UserUpdateRequestDTO(
     @Size(max = 100, message = "The name can have a maximum of 100 characters")
     String name,
-
-    @Size(max = 100, message = "The name can have a maximum of 100 characters")
-    String username,
-    
-    @Email(message = "Email must be a valid email address")
-    String email,
     
     @Length(min = 14, message = "Password must be at least 14 characters long")
     @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[^A-Za-z0-9]).+$", message = "The password must contain at least one uppercase letter, one lowercase letter, one number, and one special character")
