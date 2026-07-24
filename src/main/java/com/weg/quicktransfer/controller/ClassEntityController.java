ba0d892254c74cff69aa5674254eb358141062ca
BrunoAthanazio <athanazioferreirabruno@gmail.com>
2026-07-24 10:13:42 -0300

    feat: findAll method

--- src/main/java/com/weg/quicktransfer/controller/ClassEntityController.java
+++ src/main/java/com/weg/quicktransfer/controller/ClassEntityController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/class")
 @RequiredArgsConstructor
@@ -28,5 +30,10 @@ public class ClassEntityController {
     @GetMapping("find/name/{name}")
     public ResponseEntity<ClassEntityResponseDTO> findClassEntityByAcronym(@PathVariable String name){
         return ResponseEntity.status(HttpStatus.OK).body(classEntityService.findByAcronym(name));
+    }
+
+    @GetMapping("find/all")
+    public ResponseEntity<List<ClassEntityResponseDTO>> findAllClassEntities(){
+        return ResponseEntity.status(HttpStatus.OK).body(classEntityService.findAll());
     }
 }
