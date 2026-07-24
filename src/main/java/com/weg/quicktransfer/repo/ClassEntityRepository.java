73297227a839ec0a0ed125a119ce620ba109e902
BrunoAthanazio <athanazioferreirabruno@gmail.com>
2026-07-24 10:06:57 -0300

    feat: findClassEntityByAcronym on repository

--- src/main/java/com/weg/quicktransfer/repo/ClassEntityRepository.java
+++ src/main/java/com/weg/quicktransfer/repo/ClassEntityRepository.java
@@ -3,6 +3,10 @@ package com.weg.quicktransfer.repo;
 import com.weg.quicktransfer.model.ClassEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClassEntityRepository extends JpaRepository<ClassEntity, Long> {
     boolean existsByAcronym(String acronym);
+
+    Optional<ClassEntity> findByAcronym(String acronym);
 }
