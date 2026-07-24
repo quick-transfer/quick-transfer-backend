577af7c945e2891597b84a3c7fad0aab55036fe7
Eduardo Tamborelli Ferreira Lino <eduardo_lino@estudante.sesisenai.org.br>
2026-07-24 13:07:13 -0300

    Remove unused import in CoordinatorRepository
    Removed unused Optional import from CoordinatorRepository.

--- src/main/java/com/weg/quicktransfer/repo/CoordinatorRepository.java
+++ src/main/java/com/weg/quicktransfer/repo/CoordinatorRepository.java
@@ -4,7 +4,6 @@ import com.weg.quicktransfer.model.Coordinator;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CoordinatorRepository extends JpaRepository<Coordinator, Long> {
     boolean existsByUsername(String username);
